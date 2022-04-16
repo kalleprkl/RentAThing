20ae71dcd7581b1a89cd2deb7cd21b7efbf2b468
kp <kalle.perkoila@gmail.com>
2022-04-16 11:42:54 +0300

    swagger

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<RentalContext>(opt =>
     opt.UseInMemoryDatabase("RentAThing"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+}
 
 app.Run();
